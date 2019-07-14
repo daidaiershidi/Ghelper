f38c72fdd3f1940a5c9a083eed6da25d50def6dd
daidaiershidi <1154864382@qq.com>
2019-07-14 22:53:16 +0800

    1

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ for tmp in cookies_list:
     name = tmp.split('=',1)[0]
     value = tmp.split('=',1)[1]
     cookies[name] = value
-###################
-#获取十分钟邮箱账号##
-##################
 def apply():
     mail_front = []
     i = 0
@@ -33,27 +30,20 @@ def apply():
     mail_front = "".join(mail_front)
     mail = mail_front + '@899079.com'
     apply_data = {'mail':mail}
-    #print(data)
     
     r_apply = requests.post(apply_url,headers=headers, data=apply_data, cookies=cookies)
     assert json.loads(r_apply.content)['success']=='true'
     return mail,mima
-######################
-#是否获取邮箱的邮件内容##
-#####################
 def ifget_mail(mail):
     get_url = 'http://mail.bccto.me/getmail'
     get_data = {'mail':mail}
     r_get = requests.post(get_url, headers=headers, data=get_data, cookies=cookies)
-#    print(r_get.content.decode())
     return json.loads(r_get.content)['mail']
 def find_mail(mail,get_mail_url_last):
     url = 'http://mail.bccto.me/win/'+mail.split('@')[0]+'(a)899079-_-com/' + get_mail_url_last
-#    print(url)
     r_find = requests.get(url, headers=headers, cookies=cookies)
     www = r_find.content.decode().split('号')[1].split('官')[0].split('>')[3].split('<')[0]
     r_apply = requests.get(www, headers=headers, cookies=cookies)
-#    print(r_apply)
     return www
 def register(mail,mima):
     data = {
@@ -65,7 +55,6 @@ def register(mail,mima):
     time_ = time.time()
     url = 'https://www.chromehelper.net/server/account/signup?gtoken=5c35583e6994af913318c5350ea8a72b&version=1.4.3&proxy_pac_key=7e99c7debee0aa8a586862568830f556&api_url=https%3A%2F%2Fwww.chromehelper.net&lang=zh-CN&client_id=nonmafimegllfoonjgplbabhmgfanaka&time=' + str(time_)
     r = requests.post(url, headers=headers, data=data, cookies=cookies)
-#    print(r.content.decode())
 def login(mail, mima):
     time_ = time.time()   
     url = 'https://www.speedstunnel.com/server/account/login?gtoken=5c35583e6994af913318c5350ea8a72b&version=1.4.3&proxy_pac_key=9fe764f0b5f3a45dccf8712332b8eb5d&api_url=https%3A%2F%2Fwww.speedstunnel.com&lang=zh-CN&client_id=nonmafimegllfoonjgplbabhmgfanaka&time=' + str(time_)
@@ -74,7 +63,6 @@ def login(mail, mima):
             'password':mima
             }
     r = requests.post(url, headers=headers, data=data, cookies=cookies)
-    print(r)
 if __name__ == '__main__':
     mail,mima = apply()
     register(mail,mima)
@@ -82,16 +70,7 @@ if __name__ == '__main__':
     while(len(if_get_mail)==0):
         if_get_mail = ifget_mail(mail)
     find_mail(mail, if_get_mail[0][-2])    
-    print(mail+'\n'+mima)
+#    print(mail+'\n'+mima)
     login(mail, mima)
-#    register('h0ew3rys@899079.com','11112222')
-#    
-#    if_get_mail = ifget_mail('h0ew3rys@899079.com')
-#    while(len(if_get_mail)==0):
-#        if_get_mail = ifget_mail('h0ew3rys@899079.com')
-#    find_mail(if_get_mail[0][-2])
-#    i=0
-#    while(i<100):
-#        register('ollxdz6q@899079.com','11111123')
-#    
+   
     
